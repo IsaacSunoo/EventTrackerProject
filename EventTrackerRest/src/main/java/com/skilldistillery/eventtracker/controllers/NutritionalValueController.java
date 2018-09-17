4ce1ad3f0c8a77cf28d367f15d4a6297f0f8de6c
Isaac <Isaacsunoo@gmail.com>
2018-09-16 21:16:40 -0600

    functioning

--- EventTrackerRest/src/main/java/com/skilldistillery/eventtracker/controllers/NutritionalValueController.java
+++ EventTrackerRest/src/main/java/com/skilldistillery/eventtracker/controllers/NutritionalValueController.java
@@ -3,8 +3,11 @@ package com.skilldistillery.eventtracker.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,19 +18,55 @@ import com.skilldistillery.eventtracker.services.NutritionalValueService;
 @RestController
 @RequestMapping("api")
 public class NutritionalValueController {
-	
+
 	@Autowired
 	private NutritionalValueService nvServ;
-	
-	@RequestMapping(path="nutritionalvalues", method = RequestMethod.GET)
+
+	@RequestMapping(path = "nutritionalvalues", method = RequestMethod.GET)
 	public List<NutritionalValue> index() {
 		return nvServ.getAll();
 	}
-	
-	@RequestMapping(path="nutritionalvalues/{nid}")
+
+	@RequestMapping(path = "nutritionalvalues/{nid}")
 	public Optional<NutritionalValue> show(@PathVariable int nid) {
 		return nvServ.showOne(nid);
 	}
 
-	
+	@RequestMapping(path = "nutritionalvalues", method = RequestMethod.POST)
+	public NutritionalValue create(@RequestBody NutritionalValue nutritionVal, HttpServletResponse resp) {
+
+		NutritionalValue nv = nvServ.createNewFood(nutritionVal);
+
+		if (nv == null) {
+			resp.setStatus(500);
+		} else {
+			resp.setStatus(201);
+		}
+
+		return nv;
+	}
+
+	@RequestMapping(path = "nutritionalvalues/{fid}", method = RequestMethod.PUT)
+	public NutritionalValue replace(@PathVariable Integer fid, @RequestBody NutritionalValue nv) {
+		return nvServ.replaceFood(nv, fid);
+	}
+
+	@RequestMapping(path = "nutritionalvalues/{fid}", method = RequestMethod.DELETE)
+	public Boolean deleteFood(@PathVariable int fid, HttpServletResponse resp) {
+
+		Boolean deleted = nvServ.deleteFood(fid);
+
+		if (deleted) {
+			resp.setStatus(200);
+		} else {
+			resp.setStatus(404);
+		}
+		return deleted;
+	}
+
+	@RequestMapping(path = "nutritionalvalues/{fid}", method = RequestMethod.PATCH)
+	public NutritionalValue update(@PathVariable int fid, @RequestBody NutritionalValue nv) {
+		return nvServ.updateFood(nv, fid);
+
+	}
 }
